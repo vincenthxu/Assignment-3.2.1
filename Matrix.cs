6bdca6717b1139f4702356122bb16cc35155a206
Vincent Xu <vincent.xu1@outlook.com>
2025-07-22 09:11:50 -0700

    Removed Max property from Matrix class and added additional constructor

---
+++ Matrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Assignment_3._2._1
+{
+    public class Matrix
+    {
+        public int[,] Values { get; set; }
+        public Matrix(int[,] values) 
+        {
+            Values = values;
+        }
+        public Matrix(Random rng, int row = 2, int column = 2, int maxValue = int.MaxValue)
+        {
+            Values = new int[row, column];
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < column; c++)
+                {
+                    Values[r, c] = rng.Next(maxValue);
+                }
+            }
+        }
+        public void Print(int padLength = 0, char padChar = ' ')
+        {
+            for (int row = 0; row < Values.GetLength(0); row++)
+            {
+                for (int col = 0; col < Values.GetLength(1); col++)
+                {
+                    Console.Write($"|{Values[row, col].ToString().PadLeft(padLength, padChar)}");
+                }
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,33 +6,9 @@ namespace Assignment_3._2._1
     {
         static void Main(string[] args)
         {
-            int max = 1000;
-            int[,] matrix = Generate2DArray(row: 10, column: 10, max: max);
-            Print2DArray(matrix, padLength: max.ToString().Length);
-        }
-        static void Print2DArray(int[,] matrix, int padLength = 0, char padChar = ' ')
-        {
-            for(int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    Console.Write($"|{matrix[row, col].ToString().PadLeft(padLength, padChar)}");
-                }
-                Console.WriteLine("|");
-            }
-        }
-        static int[,] Generate2DArray(int row = 2, int column = 2, int max = int.MaxValue)
-        {
-            int[,] matrix = new int[row, column];
-            Random rng = new Random(max);
-            for(int r = 0; r < row; r++)
-            {
-                for(int c = 0; c < column; c++)
-                {
-                    matrix[r, c] = rng.Next(max);
-                }
-            }
-            return matrix;
+            int maxValue = 10000;
+            Matrix matrix = new(new Random(), row: 10, column: 10, maxValue: maxValue);
+            matrix.Print(padLength: maxValue.ToString().Length);
         }
     }
 }
